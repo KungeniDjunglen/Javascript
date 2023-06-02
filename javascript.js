bb72b8fca8533268446cbf16b916fe4d5b247b50
wabe05001 <wabe05001@utb.vaxjo.se>
2023-06-02 09:01:42 +0200

    Gjort allt

---
+++ javascript.js
@@ -0,0 +1,10 @@
+let text = "Tjena Tjena, så var det med det. ";
+
+function skriv(){
+    let p = document.getElementById("p");
+    p.innerHTML += text;
+}
+
+function alerta(){
+    alert(text);
+}
