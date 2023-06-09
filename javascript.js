af39a32c883d306d2cc8341f5a082208370c9a0b
KungeniDjunglen <thhe05001@utb.vaxjo.se>
2023-06-09 16:15:18 +0200

    Fixat den semantiskt korrekt

--- javascript.js
+++ javascript.js
@@ -1,10 +1,15 @@
 let text = "Tjena Tjena, så var det med det. ";
 
+let skrivKnapp = document.getElementById("skriv");
+skrivKnapp.addEventListener("click", skriv);
 function skriv(){
     let p = document.getElementById("p");
     p.innerHTML += text;
 }
 
+
+let alertKnapp = document.getElementById("alerta");
+alertKnapp.addEventListener("click", alerta);
 function alerta(){
     alert(text);
 }
